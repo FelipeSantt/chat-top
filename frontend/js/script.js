4c56b2dd76c87f69e78b5f31072750b599a65eb5
Felipe Santos <felipek2267@gmail.com>
2024-01-02 23:19:41 -0300

    subindo aplicação

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -88,7 +88,7 @@ const handleLogin = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://chat-tp.onrender.com");
     websocket.onmessage = processMessage
 }
 
